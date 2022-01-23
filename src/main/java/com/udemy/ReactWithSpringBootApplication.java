c91b5fe72fe29a49f613920870bf09aec7e6f286
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-23 19:45:31 -0300

    Alterando base do projeto

--- src/main/java/com/udemy/ReactWithSpringBootApplication.java
+++ src/main/java/com/udemy/ReactWithSpringBootApplication.java
@@ -1,13 +1,26 @@
 package com.udemy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.udemy.config.FileStorageConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
+@EnableAutoConfiguration
+@ComponentScan
 public class ReactWithSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactWithSpringBootApplication.class, args);
+		
+		/*BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		String result = bCryptPasswordEncoder.encode("admin123");
+		System.out.println("My hash " + result);*/
 	}
-
 }
---
+++ src/main/java/com/udemy/services/UserServices.java
@@ -0,0 +1,32 @@
+package com.udemy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.udemy.repository.UserRepository;
+
+@Service
+public class UserServices implements UserDetailsService {
+	
+	@Autowired
+	UserRepository repository;
+	
+	public UserServices(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = repository.findByUsername(username);
+		if (user != null) {
+			return user;
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found");
+		}
+		
+	}
+		
+}
